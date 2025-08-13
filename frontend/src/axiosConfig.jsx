4f214d4d5059945fa449c49a53a71b67b8e9a309
Amanda Bayaraa <n12202207@qut.edu.au>
2025-08-13 19:50:04 +1000

    change public URL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://3.27.194.18:5001/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
